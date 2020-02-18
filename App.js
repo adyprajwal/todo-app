61d7ee7cb771ec2ac21a9a18ef78ab8a695122a2
adyprajwal <prajwal.ady@gmail.com>
2020-02-18 15:53:30 +0545

    add todoitem component

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { StyleSheet, Text, View, FlatList } from "react-native";
 import Header from "./components/header";
+import TodoItem from "./components/todoitem";
 
 export default function App() {
 	const [todos, setTodos] = useState([
@@ -16,7 +17,7 @@ export default function App() {
 				<View style={styles.list}>
 					<FlatList
 						data={todos}
-						renderItem={({ item }) => <Text>{item.text}</Text>}
+						renderItem={({ item }) => <TodoItem item={item} />}
 					/>
 				</View>
 			</View>
