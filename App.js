8a3c4cdf409bf107b8f1921e5256aa0cf4c2e2ea
adyprajwal <prajwal.ady@gmail.com>
2020-02-19 14:33:22 +0545

    add flexbox

--- App.js
+++ App.js
@@ -1,5 +1,13 @@
 import React, { useState } from "react";
-import { StyleSheet, Text, View, FlatList, Alert } from "react-native";
+import {
+	StyleSheet,
+	Text,
+	View,
+	FlatList,
+	Alert,
+	TouchableWithoutFeedback,
+	Keyboard
+} from "react-native";
 import Header from "./components/header";
 import TodoItem from "./components/todoitem";
 import AddTodo from "./components/addTodo";
@@ -35,20 +43,29 @@ export default function App() {
 	};
 
 	return (
-		<View style={styles.container}>
-			<Header />
-			<View style={styles.content}>
-				<AddTodo submitHandler={submitHandler} />
-				<View style={styles.list}>
-					<FlatList
-						data={todos}
-						renderItem={({ item }) => (
-							<TodoItem item={item} pressHandler={pressHandler} />
-						)}
-					/>
+		<TouchableWithoutFeedback
+			onPress={() => {
+				Keyboard.dismiss();
+			}}
+		>
+			<View style={styles.container}>
+				<Header />
+				<View style={styles.content}>
+					<AddTodo submitHandler={submitHandler} />
+					<View style={styles.list}>
+						<FlatList
+							data={todos}
+							renderItem={({ item }) => (
+								<TodoItem
+									item={item}
+									pressHandler={pressHandler}
+								/>
+							)}
+						/>
+					</View>
 				</View>
 			</View>
-		</View>
+		</TouchableWithoutFeedback>
 	);
 }
 
@@ -58,9 +75,11 @@ const styles = StyleSheet.create({
 		backgroundColor: "#fff"
 	},
 	content: {
+		flex: 1,
 		padding: 40
 	},
 	list: {
+		flex: 1,
 		marginTop: 20
 	}
 });
