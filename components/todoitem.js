1605be00f132cee1504a9cd57b50bc463a83bf45
adyprajwal <prajwal.ady@gmail.com>
2020-02-19 16:16:41 +0545

    add delete button

--- components/todoitem.js
+++ components/todoitem.js
@@ -1,10 +1,19 @@
 import React from "react";
-import { StyleSheet, Text, TouchableOpacity } from "react-native";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import { MaterialIcons } from "@expo/vector-icons";
 
 export default function TodoItem({ item, pressHandler }) {
 	return (
-		<TouchableOpacity onPress={() => pressHandler(item.key)}>
-			<Text style={styles.item}>{item.text}</Text>
+		<TouchableOpacity>
+			<View style={styles.item}>
+				<MaterialIcons
+					name="delete"
+					size={25}
+					color="#333"
+					onPress={() => pressHandler(item.key)}
+				/>
+				<Text style={styles.itemText}>{item.text}</Text>
+			</View>
 		</TouchableOpacity>
 	);
 }
@@ -15,6 +24,10 @@ const styles = StyleSheet.create({
 		borderColor: "#bbb",
 		borderWidth: 1,
 		borderStyle: "dashed",
-		borderRadius: 10
+		borderRadius: 10,
+		flexDirection: "row"
+	},
+	itemText: {
+		marginLeft: 15
 	}
 });
