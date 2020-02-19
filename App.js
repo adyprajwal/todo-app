cae188decbd0a53e7a1acfecbad848d33df39544
adyprajwal <prajwal.ady@gmail.com>
2020-02-19 12:29:40 +0545

    add form submit handler

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { StyleSheet, Text, View, FlatList } from "react-native";
 import Header from "./components/header";
 import TodoItem from "./components/todoitem";
+import AddTodo from "./components/addTodo";
 
 export default function App() {
 	const [todos, setTodos] = useState([
@@ -15,12 +16,20 @@ export default function App() {
 			return prevTodos.filter(todo => todo.key != key);
 		});
 	};
+	const submitHandler = text => {
+		setTodos(prevTodos => {
+			return [
+				{ text: text, key: Math.random().toString() },
+				...prevTodos
+			];
+		});
+	};
 
 	return (
 		<View style={styles.container}>
 			<Header />
 			<View style={styles.content}>
-				{/*to form */}
+				<AddTodo submitHandler={submitHandler} />
 				<View style={styles.list}>
 					<FlatList
 						data={todos}
